86e14e22a25b55bc634a10e1676e214941a3b37d
Abdelghafour Lahrache <61706790+Abdelghafour2001@users.noreply.github.com>
2023-02-24 12:00:32 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         //origin: "http://localhost:3000",
-        origin: "https://XO.herokuapp.com/",
+        origin: "https://xo-abdel.herokuapp.com/",
         methods: ["GET", "POST"],
     }
 })
